b29f5fedba88bca3c845bb27cc6ecb277cc3b8ab
Silet101 <troten101@gmail.com>
2021-02-28 21:20:09 -0700

    Swapping Code
    swapped the code from funtions findFactorial() and reverseNumber() with one another.

--- GitDemo.java
+++ GitDemo.java
@@ -23,6 +23,8 @@ public class GitDemo {
      *  the function will return the value of (1 * 2 * 3 * 4)
      *****/
     public double findFactorial(int num) {
+        
+        
         double newNumber = 1;
         
         for(int i = 1; i <= num; ++i){
@@ -40,7 +42,9 @@ public class GitDemo {
      *****/
     public double reverseNumber(int num){
         
-        double factorialNumber = 0;
+
+        
+                double factorialNumber = 0;
         
         while( num != 0 ) {
             double temp = num % 10;
